30f86a82a17570b5fa41aee670e387629f0432c1
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-07-24 20:16:25 -0400

    changes
    1) There is no ESR release.  
    2) Since the daily requires assistance, the option to install everything at once is not possible.

--- 64bit/sub-menu64.sh
+++ 64bit/sub-menu64.sh
@@ -12,11 +12,9 @@ do
  echo "1. Mozilla Thunderbird";
  echo "2. Thunderbird Beta";
  echo "3. Thunderbird Daily";
- echo "4. Thunderbird for Organizations";
- echo "5. Install ALL 64-bit editions";
- echo "6. Exit";
+ echo "4. Exit";
  echo;
- echo "Please enter option [1 - 6]";
+ echo "Please enter option [1 - 4]";
  read -r opt
  case $opt in
 # Stable
@@ -25,12 +23,8 @@ do
   2) clear; echo; echo "You selected Thunderbird Beta"; echo; chmod +x ./64bit/thunderbird-beta64.sh; ./64bit/thunderbird-beta64.sh; break ;;
 # Nightly
   3) clear; echo; echo "You selected Thunderbird Daily"; echo; chmod +x ./64bit/thunderbird-nighly64.sh; ./64bit/thunderbird-nightly64.sh; break ;;
-# Extended support release
-  4) clear; echo; echo "You selected Thunderbird for Organizations"; echo; chmod +x ./64bit/thunderbird-esr64.sh; ./64bit/thunderbird-esr64.sh; break ;;
-# Everything
-  5) clear; echo; echo "You selected to install ALL 64-bit editions"; echo; chmod +x ./64bit/thunderbird-all.sh; ./64bit/thunderbird-all.sh; break ;;
 # User exit
-  6) clear; echo; echo "Goodbye, $USER"; echo; exit 1;;
+  4) clear; echo; echo "Goodbye, $USER"; echo; exit 1;;
 # invalid option notice
   *) echo "$opt is an invaild option. Please select option between 1-6 only";
      echo "Press the [enter] key to continue. . .";
